e82a7929137f0b4f1641f8dd4109c29e56a3ae2d
EC2 Default User <ec2-user@ip-172-31-4-251.ap-southeast-1.compute.internal>
2020-04-23 07:50:18 +0000

    完成 herokuにup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,16 +3,7 @@ class UsersController < ApplicationController
     def show
         @user = User.find(params[:id])
         @comment = @user.comments
-        # ＠userを優先して使用したいが上手くいくかわからない為@imageと＠nicknameを残す
-        # @bookreviews = user.bookreview.page(params[:page]).per(5).order("created_at DESC")
     end
-    
-    # def destroy
-    # tweet = Tweet.find(params[:id])
-    #     if tweet.user_id == current_user.id
-    #       tweet.destroy
-    #     end
-    # end
     
     def edit
     end
