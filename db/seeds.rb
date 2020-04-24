ca074a06588cbaba4c968ce48044834a8f8eb55e
EC2 Default User <ec2-user@ip-172-31-4-251.ap-southeast-1.compute.internal>
2020-04-24 14:14:15 +0000

    add csv

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,5 +2,5 @@ require "csv"
 
 products_csv = CSV.readlines("db/users.csv")
 products_csv.each do |row|
-  Product.create(title: row[1], image_url: row[2], director: row[5], detail: row[6], open_date: row[7])
+  Product.create(email: row[1], encrypted_password: row[2], created_at: row[5], updated_at: row[6],nickname: row[7],text: row[8])
 end
