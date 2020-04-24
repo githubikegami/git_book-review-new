d6115e44ae5e66750bcfac81df40bee50b3b480f
EC2 Default User <ec2-user@ip-172-31-4-251.ap-southeast-1.compute.internal>
2020-04-24 12:00:39 +0000

    add csv

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,6 @@
-User.create(email: 'tech-camp@gmail.com', password: '12345678', nickname: 'Mako')
+require "csv"
+
+products_csv = CSV.readlines("db/users.csv")
+products_csv.each do |row|
+  Product.create(title: row[1], image_url: row[2], director: row[5], detail: row[6], open_date: row[7])
+end
