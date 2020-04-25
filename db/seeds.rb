8cb9e7c0a1b39c20181ddf332b066ff76c266787
EC2 Default User <ec2-user@ip-172-31-4-251.ap-southeast-1.compute.internal>
2020-04-25 09:28:13 +0000

    add user seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,8 @@
 require "csv"
 
-products_csv = CSV.readlines("db/bookreviews.csv")
+products_csv = CSV.readlines(Rails.root + "db/bookreviews.csv")
 products_csv.each do |row|
-  Product.create(title: row[1], description: row[2], price: row[3], publisher: row[4],release_date: row[5],image_url: row[6],author: row[7],created_at: row[8],updated_at: row[9])
+  Bookreview.create(title: row[1], description: row[2], price: row[3], publisher: row[4],release_date: row[5],image_url: row[6],author: row[7],created_at: row[8],updated_at: row[9])
 end
+
+
