ae70a35bd69d53c94aca40a3c3e117becc967a90
EC2 Default User <ec2-user@ip-172-31-4-251.ap-southeast-1.compute.internal>
2020-04-26 12:58:58 +0000

    edit for upload to heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,5 @@ require "csv"
 
 products_csv = CSV.readlines(Rails.root + "db/bookreviews.csv")
 products_csv.each do |row|
-  Bookreview.create(title: row[1], description: row[2], price: row[3], publisher: row[4],release_date: row[5],image_url: row[6],author: row[7],created_at: row[8],updated_at: row[9])
+  Bookreview.create(id: row[0],title: row[1], description: row[2], price: row[3], publisher: row[4],release_date: row[5],image_url: row[6],author: row[7],created_at: row[8],updated_at: row[9])
 end
-
-
