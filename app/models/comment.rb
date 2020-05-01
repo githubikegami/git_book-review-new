9855864de539cec246fb2283a0fe61fd9495a64b
EC2 Default User <ec2-user@ip-172-31-4-251.ap-southeast-1.compute.internal>
2020-05-01 10:16:31 +0000

    modelにdependentを追加で記述により空欄をokにする

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,4 @@
 class Comment < ApplicationRecord
-    belongs_to :bookreview
-    belongs_to :user
+    belongs_to :bookreview, dependent: :destroy
+    belongs_to :user, dependent: :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :bookreviews
-  has_many :comments
+  has_many :bookreviews, dependent: :destroy
+  has_many :comments, dependent: :destroy
   
   has_one_attached :image
 end
