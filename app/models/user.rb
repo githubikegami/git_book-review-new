3e3be06637687ae0542911b15dbedb332f34eafb
EC2 Default User <ec2-user@ip-172-31-4-251.ap-southeast-1.compute.internal>
2020-05-01 13:58:05 +0000

    user、modelにvalidatesを追加2

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  validates :email,:password,:password_confirmation,:nickname,:image, presence: true
+  validates :email,:password,:password_confirmation,:nickname, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -8,4 +8,15 @@ class User < ApplicationRecord
   has_many :comments
   
   has_one_attached :image
+  validate :image_presence
+
+  def image_presence
+    if image.attached?
+      if !image.content_type.in?(%('image/jpeg image/png'))
+        errors.add(:image, 'にはjpegまたはpngファイルを添付してください')
+      end
+    else
+      errors.add(:image, 'ファイルを添付してください')
+    end
+  end
 end
